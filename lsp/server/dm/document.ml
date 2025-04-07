dcdb75e7f256131949caa6757415c563d68faa9d
bloomwayz <bloomwayz@snu.ac.kr>
2025-04-08 08:40:09 +0900

    :zap: Alpha-convert expressions before inference

--- lsp/server/dm/document.ml
+++ lsp/server/dm/document.ml
@@ -8,9 +8,38 @@
 open Yojson.Safe.Util
 open Lang_m
 
+module Tyenv = Poly_checker.Tyenv
+
+module Aenv = struct
+  type t = Syntax.id -> avar
+  and avar = string
+
+  let count = ref 0
+
+  let new_avar () =
+    incr count;
+    "#" ^ (string_of_int !count)
+
+  let empty : t = fun x -> x
+
+  let init () : t =
+    count := 0; empty
+
+  let bind (env : t) (id, avar : Syntax.id * avar) =
+    fun x -> if x = id then avar else (env x)
+
+  let bind_new (env : t) (id : Syntax.id) : (avar * t) =
+    let s = new_avar () in
+    s, bind env (id, s)
+end
+
 module States = struct
   type t = (string, state) Hashtbl.t
-  and state = { rawState : string; parsedState : pstate }
+  and state = {
+    rawState : string;
+    parsedState : pstate;
+    typeState : Tyenv.t;
+  }
   and pstate = Ast of Syntax.expr | Fail of string * int * int
 
   let init () : t = Hashtbl.create 39
@@ -18,27 +47,95 @@ module States = struct
   let parse_with_error (lexbuf : Lexing.lexbuf) : Syntax.expr =
     Parser.prog Lexer.read lexbuf
 
+  let rec convert (exp : Syntax.expr) (env : Aenv.t) =
+    match exp.desc with
+    | Const _ | Read -> exp
+    | Var id -> { exp with desc = Var id }
+    | Fn (x, e) ->
+      let s, env' = Aenv.bind_new env x in
+      let e' = convert e env' in
+      { exp with desc = Fn (s, e') }
+    | App (e1, e2) ->
+      let e1' = convert e1 env in
+      let e2' = convert e2 env in
+      { exp with desc = App (e1', e2') }
+    | Let (Val (x, e1), e2) ->
+      let s, env' = Aenv.bind_new env x in
+      let e1' = convert e1 env' in
+      let e2' = convert e2 env' in
+      { exp with desc = Let (Val (s, e1'), e2') }
+    | Let (Rec (f, x, e1), e2) ->
+      let s1, env' = Aenv.bind_new env f in
+      let s2, env'' = Aenv.bind_new env' x in
+      let e1' = convert e1 env'' in
+      let e2' = convert e2 env' in
+      { exp with desc = Let (Rec (s1, s2, e1'), e2') }
+    | If (e0, e1, e2) ->
+      let e0' = convert e1 env in
+      let e1' = convert e1 env in
+      let e2' = convert e2 env in
+      { exp with desc = If (e0', e1', e2') }
+    | Bop (op, e1, e2) ->
+      let e1' = convert e1 env in
+      let e2' = convert e2 env in
+      { exp with desc = Bop (op, e1', e2') }
+    | Write e -> { exp with desc = Write (convert e env) }
+    | Malloc e -> { exp with desc = Malloc (convert e env) }
+    | Assign (e1, e2) ->
+      let e1' = convert e1 env in
+      let e2' = convert e2 env in
+      { exp with desc = Assign (e1', e2') }
+    | Deref e -> { exp with desc = Deref (convert e env) }
+    | Seq (e1, e2) ->
+      let e1' = convert e1 env in
+      let e2' = convert e2 env in
+      { exp with desc = Seq (e1', e2') }
+    | Pair (e1, e2) ->
+      let e1' = convert e1 env in
+      let e2' = convert e2 env in
+      { exp with desc = Pair (e1', e2') }
+    | Fst e -> { exp with desc = Fst (convert e env) }
+    | Snd e -> { exp with desc = Snd (convert e env) }
+
   let get_pstate (filename : string) (rstate : string) : pstate =
     let lexbuf = Lexing.from_string rstate in
     lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename };
 
     match parse_with_error lexbuf with
-    | ast -> Ast ast
+    | ast ->
+      let aenv = Aenv.init () in
+      Ast (convert ast aenv)
     | exception Parser.Error ->
         let open Lexing in
         let pos = lexbuf.lex_curr_p in
-        Fail ("Parsing Error", pos.pos_lnum, pos.pos_cnum - pos.pos_bol + 1)
+        let _, ln, col = Location.get_pos_info pos in
+        Fail ("Parsing Error", ln, col)
     | exception Lexer.SyntaxError msg ->
         let open Lexing in
         let pos = lexbuf.lex_curr_p in
-        Fail ("Parsing Error", pos.pos_lnum, pos.pos_cnum - pos.pos_bol + 1)
+        Fail (msg, pos.pos_lnum, pos.pos_cnum - pos.pos_bol + 1)
+
+  let get_tstate (pstate : pstate) : Tyenv.t =
+    let open Poly_checker in
+    match pstate with
+    | Ast exp ->
+      let tyenv = Tyenv.empty in
+      let a = new_var () in
+      fst (infer tyenv a exp)
+    | Fail _ -> Tyenv.empty
 
   (* synchronize functions *)
   let update (states : t) (uri : string) (raw : string) =
     let uri_len = String.length uri in
     let fname = String.sub uri 8 (uri_len - 8) in
     let parsed = get_pstate fname raw in
-    let st = { rawState = raw; parsedState = parsed } in
+    let tyenv = get_tstate parsed in
+    let st = {
+      rawState = raw;
+      parsedState = parsed;
+      typeState = tyenv;
+    }
+    in
 
     match Hashtbl.find_opt states uri with
     | Some _ -> Hashtbl.replace states uri st
